7f7550314fc5f3020a5f13221befca692e5a0ce4
MarvinJWendt <git@marvinjwendt.com>
2022-06-02 01:59:23 +0200

    fixed description styling

--- doc.go
+++ doc.go
@@ -1,6 +1,8 @@
 /*
 Package keyboard can be used to read key presses from the keyboard, while in a terminal application. It's crossplatform and keypresses can be combined.
 
+```go
+
   keyboard.StartListener()
   defer keyboard.StopListener()
 
@@ -14,5 +16,7 @@ Package keyboard can be used to read key presses from the keyboard, while in a t
 
   	fmt.Println("\r", keyInfo.String())
   }
+
+```
 */
 package keyboard
