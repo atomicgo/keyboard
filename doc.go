275475d106d0e9fbffd3a71ede18658ea0c0f1e7
MarvinJWendt <git@marvinjwendt.com>
2022-06-02 01:58:46 +0200

    fixed description styling

--- doc.go
+++ doc.go
@@ -1,18 +1,18 @@
 /*
 Package keyboard can be used to read key presses from the keyboard, while in a terminal application. It's crossplatform and keypresses can be combined.
 
-	keyboard.StartListener()
-	defer keyboard.StopListener()
+  keyboard.StartListener()
+  defer keyboard.StopListener()
 
-	for {
-		keyInfo, _ := keyboard.GetKey()
-		key := keyInfo.Code
+  for {
+  	keyInfo, _ := keyboard.GetKey()
+  	key := keyInfo.Code
 
-		if key == keys.CtrlC {
-			break
-		}
+  	if key == keys.CtrlC {
+      break
+  	}
 
-		fmt.Println("\r", keyInfo.String())
-	}
+  	fmt.Println("\r", keyInfo.String())
+  }
 */
 package keyboard
