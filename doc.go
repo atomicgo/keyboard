c405c8b70f2a1ac60cd6c524ddc69b5f92eb33b1
MarvinJWendt <git@marvinjwendt.com>
2022-06-02 02:08:27 +0200

    fixed description styling

--- doc.go
+++ doc.go
@@ -1,5 +1,7 @@
 /*
-Package keyboard can be used to read key presses from the keyboard, while in a terminal application. It's crossplatform and keypresses can be combined.
+Package keyboard can be used to read key presses from the keyboard, while in a terminal application. It's crossplatform and keypresses can be combined to check for ctrl+c, alt+4, ctrl-shift, alt+ctrl+right, etc.
+
+Works nicely with https://atomicgo.dev/cursor
 
 ```go
 
